795e692587526894dd3790f99995619f564a4dd7
Mathis Frahm <mathisfrahm@gmx.de>
2022-04-25 15:32:32 +0200

    manually initialized and filled a handful of histograms

--- ap/tasks/fillHistograms.py
+++ ap/tasks/fillHistograms.py
@@ -11,6 +11,8 @@ import law
 from ap.tasks.framework import DatasetTask, HTCondorWorkflow
 from ap.util import ensure_proxy
 
+import ap.config.analysis_st as an
+
 class FillHistograms(DatasetTask, law.LocalWorkflow, HTCondorWorkflow):
 
     sandbox = "bash::$AP_BASE/sandboxes/cmssw_default.sh"
@@ -43,18 +45,120 @@ class FillHistograms(DatasetTask, law.LocalWorkflow, HTCondorWorkflow):
         print(type(events))
         print(type(selection))
 
+        print("test: readout e-channel from config")
+        print(an.config_2018.get_channel("e"))
+
+        
+        # declare output: list of histograms?
+        output = []
+
         # how to mask without changing the size of 'events':
         #events.mask[selection.trigger_sel]
         
         # for now, just apply all selections
         events = events[(selection.trigger_sel) & (selection.lep_sel) & (selection.jet_sel) & (selection.bjet_sel)]
 
-        # define variables of interest
+        # variables of interest: needs to be defined in a previous task?
         HT = ak.sum(events.Jet_pt, axis=1)
 
-        lepton_pt = np.where(events["nMuon"]==1, events["Muon_pt"], events["Electron_pt"]) # this works only if there's either only electrons or only muons
+        for var in an.config_2018.variables:
+            print(var.get_mpl_hist_data())
+        for var in an.config_2018.variables:
+            print(var.get_full_title())
 
-        # define histogram
+        lepton_pt = np.where(events["nMuon"]==1, events["Muon_pt"], events["Electron_pt"]) # this works only if there's either only electrons or only muons
+        lepton_eta = np.where(events["nMuon"]==1, events["Muon_eta"], events["Electron_eta"]) # this works only if there's either only electrons or only muons
+
+        # define histograms
+        h_nJet = (
+            hist.Hist.new
+            .Reg(8, -.5, 7.5, name="nJet", label="$N_{jet}$")
+            .Weight()
+        )
+        h_Jet1_pt = (
+            hist.Hist.new
+            .Reg(40, 0, 200, name="pt_j1", label="$p_{T, j1}$")
+            .Weight()
+        )
+        h_Jet2_pt = (
+            hist.Hist.new
+            .Reg(40, 0, 200, name="pt_j2", label="$p_{T, j2}$")
+            .Weight()
+        )
+        '''
+        h_Jet1_eta = (
+            hist.Hist.new
+            .Reg(40, 0, 200, name="eta_j1", label="$\eta_{j1}$")
+            .Weight()
+        )
+        h_Jet2_eta = (
+            hist.Hist.new
+            .Reg(40, 0, 200, name="eta_j2", label="$\eta_{j2}$")
+            .Weight()
+        )
+        '''
+        h_nLep = (
+            hist.Hist.new
+            .Reg(8, -.5, 7.5, name="nLep", label="$N_{lep}$")
+            .Weight()
+        )
+        h_Lep_pt = (
+            hist.Hist.new
+            .Reg(40, 0, 200, name="pt_lep", label="$p_{T, lep}$")
+            .Weight()
+        )
+        h_Lep_eta = (
+            hist.Hist.new
+            .Reg(40, 0, 200, name="eta_lep", label="$\eta_{lep}$")
+            .Weight()
+        )
+        h_HT = (
+            hist.Hist.new
+            .Reg(40,0,800, name="HT", label="HT")
+            .Weight()
+        )
+
+
+        # manually fill histograms
+        h_nJet.fill(
+            nJet=events["nJet"],
+            weight=1
+        )
+        h_Jet1_pt.fill(
+            pt_j1=events["Jet_pt"][:,0], # nJet>0 is given by selection
+            weight=1
+        )
+        h_Jet2_pt.fill(
+            pt_j2=events["Jet_pt"][(events.nJet>1)][:,1], # nJet>1 required to fill this histogram
+            weight=1
+        )
+
+        h_nLep.fill(
+            nLep=events["nElectron"]+events["nMuon"],
+            weight=1
+        )
+        h_Lep_pt.fill(
+            pt_lep=lepton_pt[:,0],
+            weight=1
+        )
+        h_Lep_eta.fill(
+            eta_lep=lepton_eta[:,0],
+            weight=1
+        )
+        h_HT.fill(
+            HT=HT,
+            weight=1
+        )
+
+        output.append(h_nJet)
+        output.append(h_Jet1_pt)
+        output.append(h_Jet2_pt)
+        output.append(h_nLep)
+        output.append(h_Lep_pt)
+        output.append(h_Lep_eta)
+        output.append(h_HT)
+
+        '''
         h_eventVars = (
             hist.Hist.new
             #.StrCat(["st", "tt", "data"], name="sample", label="Sample")
@@ -67,11 +171,6 @@ class FillHistograms(DatasetTask, law.LocalWorkflow, HTCondorWorkflow):
             .Reg(40, 0, 200, name="pt_lep", label="$p_{T, lep}$")
             .Weight()
         )
-
-        # h_jets
-        # h_jet1, h_jet2, ...
-        # h_lep?
-        
         h_eventVars.fill(
             #sample="st", # sample type should be read out here using Order
             N_jet=events["nJet"],
@@ -83,8 +182,9 @@ class FillHistograms(DatasetTask, law.LocalWorkflow, HTCondorWorkflow):
             pt_lep=lepton_pt[:,0],
             weight=1 # dummy, should be read out here using Order
         )
-    
-        self.output().dump(h_eventVars, formatter="pickle")
+        '''
+
+        self.output().dump(output, formatter="pickle")
 
 
 
