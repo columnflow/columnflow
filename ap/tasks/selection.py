9b27dc141bd493a83d28f462588ec491a05301ac
Marcel R <github.riga@icloud.com>
2022-04-27 13:37:58 +0200

    Add CalibrateObjects dummy.

--- ap/tasks/selection.py
+++ ap/tasks/selection.py
@@ -12,6 +12,85 @@ from ap.tasks.framework import DatasetTask, HTCondorWorkflow
 from ap.util import ensure_proxy
 
 
+class CalibrateObjects(DatasetTask, law.LocalWorkflow, HTCondorWorkflow):
+
+    sandbox = "bash::$AP_BASE/sandboxes/venv_selection.sh"
+
+    def workflow_requires(self):
+        # workflow super classes might already define requirements, so extend them
+        reqs = super(CalibrateObjects, self).workflow_requires()
+        reqs["lfns"] = GetDatasetLFNs.req(self)
+        return reqs
+
+    def requires(self):
+        # workflow branches are normal tasks, so define requirements the normal way
+        return {"lfns": GetDatasetLFNs.req(self)}
+
+    def output(self):
+        return self.local_target(f"data_{self.branch}.parquet")
+
+    @law.decorator.safe_output
+    @ensure_proxy
+    def run(self):
+        import numpy as np
+        import uproot
+        import awkward as ak
+
+        # get all lfns
+        lfns = self.input()["lfns"].random_target().load(formatter="json")
+
+        # create a temp dir for saving intermediate files
+        tmp_dir = law.LocalDirectoryTarget(is_tmp=True)
+        tmp_dir.touch()
+        output_chunks = []
+
+        # loop over all input file indices requested by this branch
+        for file_index in self.branch_data:
+            # get the lfn of the file referenced by this file index
+            lfn = str(lfns[file_index])
+
+            # always use the INFN redirector for now
+            input_file = law.wlcg.WLCGFileTarget(lfn, fs="wlcg_fs_infn")
+            input_size = law.util.human_bytes(input_file.stat().st_size, fmt=True)
+            self.publish_message(f"file {file_index}: fround {lfn}, size is {input_size}")
+
+            # open with uproot
+            with self.publish_step(f"file {file_index}: load and open with uproot ..."):
+                data = input_file.load(formatter="uproot")
+                events = data["Events"]
+                self.publish_message(f"file {file_index}: found {events.num_entries} events")
+
+            with self.publish_step(f"iterating over file {file_index} ..."):
+                # optimization targets: threads and step size
+                pool = uproot.ThreadPoolExecutor(4)
+                step_size = "50MB"
+                for i, batch in enumerate(events.iterate(step_size=step_size,
+                        interpretation_executor=pool, decompression_executor=pool)):
+                    # change objects per batch here ...
+                    pass
+
+                    # dummy implementation:
+                    # scale 4-vec of jets with pt > 30 by 1.1 (phi and eta are invariant)
+                    # note for future: ak made some unintuitive design decisions about mutability
+                    # (see https://github.com/scikit-hep/awkward-1.0/discussions/609), which are
+                    # contradicting numpy / tf standards, with many consequences, one of them being
+                    # that in-place assignment is not supported, and we have to go via plain numpy
+                    # to do manipulations on flattened objects, so inconvenient syntactic mess ahead
+                    jes_mask = batch.Jet_pt > 30
+                    np.asarray(ak.flatten(batch.Jet_pt))[np.asarray(ak.flatten(jes_mask))] *= 1.1
+                    np.asarray(ak.flatten(batch.Jet_mass))[np.asarray(ak.flatten(jes_mask))] *= 1.1
+
+                    # save the batch as parquet
+                    chunk = tmp_dir.child(f"file_{file_index}_{i}.parquet", type="f")
+                    ak.to_parquet(batch, chunk.path)
+                    output_chunks.append(chunk)
+
+        # merge the files
+        with self.output().localize("w") as output:
+            law.pyarrow.merge_parquet_task(self, output_chunks, output, local=True)
+
+
+
 class SelectEvents(DatasetTask, law.LocalWorkflow, HTCondorWorkflow):
 
     sandbox = "bash::$AP_BASE/sandboxes/venv_selection.sh"
