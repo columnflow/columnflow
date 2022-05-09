40b3cbb3242d5cdede25dd379a1cff0d3eb049cb
Mathis Frahm <mathisfrahm@gmx.de>
2022-05-09 16:41:34 +0200

    added publish_step calls to check runtime of tasks

--- ap/tasks/fillHistograms.py
+++ ap/tasks/fillHistograms.py
@@ -34,90 +34,89 @@ class FillHistograms(DatasetTask, law.LocalWorkflow, HTCondorWorkflow):
     @law.decorator.safe_output
     @ensure_proxy
     def run(self):
-        import numpy as np
-        import awkward as ak
-        import hist
+        with self.publish_step("FillHistograms run ..."):
 
-        # stuff that can be used from order
-        config = self.config_inst
-        analysis = self.get_analysis_inst(self.analysis)
-        #categories = config.categories
-        #processes = config.processes
+            import numpy as np
+            import awkward as ak
+            import hist
+
+            # stuff that can be used from order
+            config = self.config_inst
+            analysis = self.get_analysis_inst(self.analysis)
+            #categories = config.categories
+            #processes = config.processes
+        
+            # need process to read out xsec
+            process = config.get_process(self.dataset)
+            # need dataset to read out number of MC events
+            dataset = config.get_dataset(self.dataset)
+
+            events = self.input()["data"].load(formatter="pickle")
+            selection = self.input()["selection"].load(formatter="pickle")
 
 
-        print("dataset: %s" % self.dataset)
-        # need process to read out xsec
-        process = config.get_process(self.dataset)
-        # need dataset to read out number of MC events
-        dataset = config.get_dataset(self.dataset)
+            mask = True
+            # just apply all selections (to fill workflow histograms, create a parallel task)
+            for sel in ak.fields(selection):
+                mask = (mask) & (selection[sel])
+            events = events[mask]
+            
 
-        events = self.input()["data"].load(formatter="pickle")
-        selection = self.input()["selection"].load(formatter="pickle")
-
-
-
-        mask = True
-        # just apply all selections (to fill workflow histograms, create a parallel task)
-        for sel in ak.fields(selection):
-            mask = (mask) & (selection[sel])
-        events = events[mask]
+            # determine which event belongs in which category
+            # this could be included in an earlier task... (part of defineSelection? Returning a mask for each category? Or an array of strings (requires the categories to be orthogonal))
+            cat_titles = []
+            cat_array = "no_cat"
+            mask_int = 0
+            for cat in self.config_inst.get_leaf_categories():
+                sel = cat.selection
+                cat_titles.append(cat.name)
+                mask = getattr(fcts, sel)(events)
+                cat_array = np.where(mask, cat.name, cat_array)
+                mask_int = mask_int + np.where(mask,1,0) # to check orthogonality of categories
+            if not ak.all(mask_int==1):
+                if ak.any(mask_int>=2):
+                    raise ValueError('Leaf categories are supposed to be fully orthogonal')
+                else:
+                    raise ValueError('Some events are without leaf category')
+                    #print('Some events are without leaf category')
         
 
-        # determine which event belongs in which category
-        # this could be included in an earlier task... (part of defineSelection? Returning a mask for each category? Or an array of strings (requires the categories to be orthogonal))
-        cat_titles = []
-        cat_array = "no_cat"
-        mask_int = 0
-        for cat in self.config_inst.get_leaf_categories():
-            sel = cat.selection
-            cat_titles.append(cat.name)
-            mask = getattr(fcts, sel)(events)
-            cat_array = np.where(mask, cat.name, cat_array)
-            mask_int = mask_int + np.where(mask,1,0) # to check orthogonality of categories
-        if not ak.all(mask_int==1):
-            if ak.any(mask_int>=2):
-                raise ValueError('Leaf categories are supposed to be fully orthogonal')
-            else:
-                raise ValueError('Some events are without leaf category')
-                #print('Some events are without leaf category')
+            # declare output: dict of histograms
+            output = {}
         
-
-        # declare output: dict of histograms
-        output = {}
-
-        sampleweight = config.x.luminosity * process.get_xsec(config.campaign.ecm).get() / dataset.n_events # get_xsec() gives not a number but number+-uncert
-        eventweight = events["LHEWeight_originalXWGTUP"] / process.get_xsec(config.campaign.ecm).get()
-        print("sampleweight = %f" %sampleweight)
-        print("eventweight = ", eventweight)
-        weight = sampleweight * eventweight
-        print("weight = ", weight)
+            sampleweight = config.x.luminosity * process.get_xsec(config.campaign.ecm).get() / dataset.n_events # get_xsec() gives not a number but number+-uncert
+            eventweight = events["LHEWeight_originalXWGTUP"] / process.get_xsec(config.campaign.ecm).get()
+            print("sampleweight = %f" %sampleweight)
+            print("eventweight = ", eventweight)
+            weight = sampleweight * eventweight
+            print("weight = ", weight)
         
-        #print(ak.all(ak.isclose(events["LHEWeight_originalXWGTUP"],events["Generator_weight"])))
-        #print(ak.all(ak.isclose(events["LHEWeight_originalXWGTUP"],events["genWeight"])))
+            #print(ak.all(ak.isclose(events["LHEWeight_originalXWGTUP"],events["Generator_weight"])))
+            #print(ak.all(ak.isclose(events["LHEWeight_originalXWGTUP"],events["genWeight"])))
 
 
-        variables = config.variables.names()
-        print("Looping over all variables in config ....")
-        for var_name in variables:
-            print(var_name)
-            var = config.variables.get(var_name)
-            h_var = (
-                hist.Hist.new
-                .StrCategory(cat_titles, name="category")
-                .Reg(*var.binning, name=var_name, label=var.get_full_x_title())
-                .Weight()
-            )
-            fill_kwargs = {
-                "category": cat_array,
-                var_name: getattr(fcts, var.expression)(events),
-                "weight": weight,
-            }
-            h_var.fill(**fill_kwargs)
-            output[var_name] = h_var
-
-
-        self.output().dump(output, formatter="pickle")
-
+            variables = config.variables.names()
+            with self.publish_step("looping over all variables in config ...."):
+                for var_name in variables:
+                    with self.publish_step("var: ", var_name):
+                        var = config.variables.get(var_name)
+                        h_var = (
+                            hist.Hist.new
+                            .StrCategory(cat_titles, name="category")
+                            .Reg(*var.binning, name=var_name, label=var.get_full_x_title())
+                            .Weight()
+                        )
+                        fill_kwargs = {
+                            "category": cat_array,
+                            var_name: getattr(fcts, var.expression)(events),
+                            "weight": weight,
+                        }
+                        h_var.fill(**fill_kwargs)
+                        output[var_name] = h_var
+                
+                
+            self.output().dump(output, formatter="pickle")
+            self.publish_message(f"FillHistograms run method done")
 
 
 # trailing imports
--- ap/tasks/mergeHistograms.py
+++ ap/tasks/mergeHistograms.py
@@ -30,46 +30,33 @@ class MergeHistograms(ForestMerge, DatasetTask, law.LocalWorkflow, HTCondorWorkf
 
     def merge_requires(self, start_leaf, end_leaf):
         return [FillHistograms.req(self, branch=i) for i in range(start_leaf, end_leaf)]
-        #return{
-        #    [process: [FillHistograms.req(self, branch=i, dataset=process.datasets()) for i in range(start_leaf, end_leaf)] for process in processes]
-        #}
 
     def merge_output(self):
         return self.local_target(f"histograms_{self.dataset}.pickle")
-        #return self.local_target(f"histograms_{self.branch_data['process']}")
-
 
     def merge(self, inputs, output):
-        #import awkward as ak
-        import hist
-        
-        merged = {}
+        with self.publish_step("Hello from MergeHistograms"):
+            import hist
+            merged = {}
+            inputs_list = [i.load(formatter="pickle") for i in inputs]
+            inputs_dict = {k:[el[k] for el in inputs_list] for k in inputs_list[0].keys()}
+            
+            for k in inputs_dict.keys():
+                print(k)
+                print(type(inputs_dict[k]))
+                h_out = inputs_dict[k][0]
+                for i,h_in in enumerate(inputs_dict[k]):
+                    if(i==0):
+                        continue
+                    print(type(h_in))
+                    h_out += h_in
+                merged[k] = h_out
 
-        # config
-        #config = self.config_inst
-
-        print(type(inputs))
-        inputs_list = [i.load(formatter="pickle") for i in inputs]
-
-        inputs_dict = {k:[el[k] for el in inputs_list] for k in inputs_list[0].keys()}
-        
-        for k in inputs_dict.keys():
-            print(k)
-            print(type(inputs_dict[k]))
-            h_out = inputs_dict[k][0]
-            for i,h_in in enumerate(inputs_dict[k]):
-                if(i==0):
-                    continue
-                print(type(h_in))
-                h_out += h_in
-            merged[k] = h_out
-
-        for k in merged.keys():
-            print(k)
-            print(type(merged[k]))
-
-
-        output.dump(merged, formatter="pickle")
+            for k in merged.keys():
+                print(k)
+                print(type(merged[k]))
+                
+            output.dump(merged, formatter="pickle")
     
 
 
