87cdb2b4c790b73538ae7dd9a568090675fdc659
Marcel R <github.riga@icloud.com>
2022-05-13 10:26:08 +0200

    Pre-initialize all wlcg file systems for cache cleanups.

--- ap/__init__.py
+++ ap/__init__.py
@@ -6,7 +6,13 @@ Main entry point for top-level settings and fixes before anything else is import
 
 import law
 
+
+# load contrib packages
 law.contrib.load(
     "arc", "awkward", "cms", "coffea", "git", "htcondor", "numpy", "pyarrow", "telegram", "root",
     "tasks", "wlcg",
 )
+
+# initialize wlcg file systems once so that their cache cleanup is triggered if configured
+wlcg_file_systems = law.config.get_expanded("target", "wlcg_file_systems", split_csv=True)
+wlcg_file_systems = list(map(law.wlcg.WLCGFileSystem, wlcg_file_systems))
