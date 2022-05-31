3a7fa9999878239626b2d7c480f83fd5b94c4dd1
Marcel R <github.riga@icloud.com>
2022-05-31 14:56:46 +0200

    Remove some global noqa flags.

--- ap/tasks/framework/__init__.py
+++ ap/tasks/framework/__init__.py
@@ -1,11 +1,10 @@
 # coding: utf-8
-# flake8: noqa
 
 """
 "tasks.framework" module, mainly for provisioning imports.
 """
 
-from ap.tasks.framework.base import (
+from ap.tasks.framework.base import (  # noqa
     AnalysisTask, ConfigTask, DatasetTask, CommandTask, wrapper_factory,
 )
-from ap.tasks.framework.remote import HTCondorWorkflow, BundleRepo, BundleSoftware
+from ap.tasks.framework.remote import HTCondorWorkflow, BundleRepo, BundleSoftware  # noqa
