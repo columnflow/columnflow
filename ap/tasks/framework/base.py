52d7bee125a9f9772fb3b27c47bde278a21b83fb
Marcel R <github.riga@icloud.com>
2022-06-04 08:58:27 +0200

    Add PlotMixing and view_output_plots decorator.

--- ap/tasks/framework/base.py
+++ ap/tasks/framework/base.py
@@ -552,6 +552,54 @@ class CommandTask(AnalysisTask):
         return
 
 
+class PlotMixin(AnalysisTask):
+
+    view_cmd = luigi.Parameter(
+        default=law.NO_STR,
+        significant=False,
+        description="a command to execute after the task has run to visualize plots right in the "
+        "terminal; no default",
+    )
+
+
+@law.decorator.factory(accept_generator=True)
+def view_output_plots(fn, opts, task, *args, **kwargs):
+    def before_call():
+        return None
+
+    def call(state):
+        return fn(task, *args, **kwargs)
+
+    def after_call(state):
+        view_cmd = getattr(task, "view_cmd", None)
+        if not view_cmd or view_cmd == law.NO_STR:
+            return
+
+        # prepare the view command
+        if "{}" not in view_cmd:
+            view_cmd += " {}"
+
+        # collect all paths to view
+        view_paths = []
+        outputs = law.util.flatten(task.output())
+        while outputs:
+            output = outputs.pop(0)
+            if isinstance(output, law.TargetCollection):
+                outputs.extend(output._flat_target_list)
+                continue
+            if not getattr(output, "path", None):
+                continue
+            if output.path.endswith((".pdf", ".png")) and output.path not in view_paths:
+                view_paths.append(output.path)
+
+        # loop through paths and view them
+        for path in view_paths:
+            task.publish_message("showing {}".format(path))
+            law.util.interruptable_popen(view_cmd.format(path), shell=True, executable="/bin/bash")
+
+    return before_call, call, after_call
+
+
 def wrapper_factory(
     base_cls: law.Task,
     require_cls: AnalysisTask,
