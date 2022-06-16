978651f876b4e8fb84aaa7e20fe5bf4acc1740b1
Marcel R <github.riga@icloud.com>
2022-06-16 17:53:08 +0200

    Adjust old code using columns from selection results.

--- ap/tasks/histograms.py
+++ ap/tasks/histograms.py
@@ -81,7 +81,9 @@ class CreateHistograms(
                 events = update_ak_array(events, *columns)
 
                 # calculate variables
-                results = variables(events)
+                # TODO: the variables callable is currently a selection function and should be a
+                #       producer soon
+                variables(events)
 
                 # weights
                 lumi = self.config_inst.x.luminosity.get("nominal")
@@ -109,11 +111,9 @@ class CreateHistograms(
                             fill_kwargs = {
                                 "category": events.cat_array,
                                 "shift": self.shift,
-                                var_name: results.columns[var_name],
+                                var_name: events[var_name],
                                 "weight": weight,
                             }
-                            print(results.columns[var_name])
-                            print(self.shift)
                             h_var.fill(**fill_kwargs)
                             if var_name in histograms:
                                 histograms[var_name] += h_var
