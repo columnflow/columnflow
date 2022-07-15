25160885a139844016a37bd57bf34ed8160ff25a
Bogdan-Wiederspan <79155113+Bogdan-Wiederspan@users.noreply.github.com>
2022-07-15 19:57:30 +0200

    Features/unit tests (#69)
    * Add dummy test module for columnar_util.
    
    * partly translate nathans notes
    
    * Adjust test env in CI jobs.
    
    * Refactor coverage and test scripts.
    
    * Completed unit tests for Route and ArrayFunction Class
    
    * fixed linting
    
    * Used delayed imports in tests.
    
    * Add single test script.
    
    * Fix test cases.
    
    Co-authored-by: Marcel R <github.riga@icloud.com>
    Co-authored-by: Nathan Bruno Emmanuel Yves Prouvost <prouvost@naf-cms16.desy.de>
    Co-authored-by: Nathan Prouvost <nathanprouvost@eduroam-1206.desy.de>
    Co-authored-by: Bogdan W <bogdan.wiederspan@desy.de>

--- sandboxes/venv_columnar_dev.sh
+++ sandboxes/venv_columnar_dev.sh
@@ -6,10 +6,11 @@
 action() {
     local this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
     local this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
+    local ap_dir="$( dirname "$this_dir" )"
 
     # set variables and source the generic venv setup
     export AP_VENV_NAME="$( basename "${this_file%.sh}" )"
-    export AP_VENV_REQUIREMENTS="$this_dir/columnar.txt,$this_dir/dev.txt"
+    export AP_VENV_REQUIREMENTS="$this_dir/columnar.txt,$ap_dir/requirements_dev.txt"
 
     source "$this_dir/_setup_venv.sh" "$@"
 }
