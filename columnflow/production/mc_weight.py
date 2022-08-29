4bc644d8c738bc29aedf12bcf85c7e381fe5d93e
Marcel R <github.riga@icloud.com>
2022-08-29 16:13:25 +0200

    Update custom mc weight getter.

--- columnflow/production/mc_weight.py
+++ columnflow/production/mc_weight.py
@@ -23,10 +23,9 @@ def mc_weight(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     where this failed.
 
     Strategy:
-      1. When LHEWeight is missing, use genWeight.
-      2. When both exist and they are identical, use genWeight.
-      3. When both exist and genWeight is always one, use LHEWeight.
-      4. Raise an exception in all other cases.
+
+      1. Use LHEWeight.originalXWGTUP when it exists and genWeight is always 1.
+      2. In all other cases, use genWeight.
 
     [1] https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookNanoAOD?rev=99#Weigths
     """
@@ -35,17 +34,9 @@ def mc_weight(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
         return events
 
     # determine the mc_weight
-    if not has_ak_column(events, "LHEWeight.originalXWGTUP"):
-        mc_weight = events.genWeight
-    elif ak.all(events.genWeight == events.LHEWeight.originalXWGTUP):
-        mc_weight = events.genWeight
-    elif ak.all(events.genWeight == 1.0):
+    mc_weight = events.genWeight
+    if has_ak_column(events, "LHEWeight.originalXWGTUP") and ak.all(events.genWeight == 1.0):
         mc_weight = events.LHEWeight.originalXWGTUP
-    else:
-        raise Exception(
-            f"cannot determine mc_weight from genWeight {events.genWeight} and "
-            f"LHEWeight {events.LHEWeight.originalXWGTUP}",
-        )
 
     # store the column
     events = set_ak_column(events, "mc_weight", mc_weight)
