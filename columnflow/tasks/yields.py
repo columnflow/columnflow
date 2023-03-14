7ed005a692e48b8334fdf7a47a78515341aba2b0
Mathis Frahm <mathisfrahm@gmx.de>
2023-03-14 16:36:40 +0100

    use event variable and allow categories as input

--- columnflow/tasks/yields.py
+++ columnflow/tasks/yields.py
@@ -9,7 +9,10 @@ from collections import OrderedDict
 import law
 import luigi
 
-from columnflow.tasks.framework.mixins import DatasetsProcessesMixin
+from columnflow.tasks.framework.mixins import (
+    CalibratorsMixin, SelectorStepsMixin, ProducersMixin,
+    DatasetsProcessesMixin, CategoriesMixin,
+)
 from columnflow.tasks.framework.remote import RemoteWorkflow
 
 from columnflow.tasks.histograms import MergeHistograms
@@ -18,7 +21,11 @@ from columnflow.util import dev_sandbox
 
 class CreateYieldTable(
     DatasetsProcessesMixin,
+    CategoriesMixin,
     law.LocalWorkflow,
+    ProducersMixin,
+    SelectorStepsMixin,
+    CalibratorsMixin,
     RemoteWorkflow,
 ):
     sandbox = dev_sandbox("bash::$CF_BASE/sandboxes/venv_columnar.sh")
@@ -31,6 +38,12 @@ class CreateYieldTable(
         description="format of the yield table, takes all fromats taken by the tabulate package; default: latex_raw",
     )
 
+    skip_variance = luigi.BoolParameter(
+        default=False,
+        significant=False,
+        description="wether to skip variance or not; default: False",
+    )
+
     # dummy branch map
     def create_branch_map(self):
         return [0]
@@ -40,7 +53,7 @@ class CreateYieldTable(
             d: self.dep_MergeHistograms.req(
                 self,
                 dataset=d,
-                variables=("mc_weight",),
+                variables=("event",),
                 _prefer_cli={"variables"},
             )
             for d in self.datasets
@@ -54,6 +67,7 @@ class CreateYieldTable(
         import hist
         from tabulate import tabulate
 
+        category_insts = list(map(self.config_inst.get_category, self.categories))
         process_insts = list(map(self.config_inst.get_process, self.processes))
         sub_process_insts = {
             proc: [sub for sub, _, _ in proc.walk_processes(include_self=True)]
@@ -63,10 +77,10 @@ class CreateYieldTable(
         # histogram data per process
         hists = {}
 
-        with self.publish_step("dummy text"):
+        with self.publish_step(f"Creating yields for processes {self.processes}, categories {self.categories}"):
             for dataset, inp in self.input().items():
                 dataset_inst = self.config_inst.get_dataset(dataset)
-                h_in = inp["mc_weight"].load(formatter="pickle")
+                h_in = inp["event"].load(formatter="pickle")
 
                 # loop and extract one histogram per process
                 for process_inst in process_insts:
@@ -87,7 +101,7 @@ class CreateYieldTable(
                     }]
 
                     # axis reductions
-                    h = h[{"process": sum, "shift": sum, "mc_weight": sum}]
+                    h = h[{"process": sum, "shift": sum, "event": sum}]
 
                     # add the histogram
                     if process_inst in hists:
@@ -106,24 +120,35 @@ class CreateYieldTable(
             )
 
             yields = []
-            yield_header = ["Process"]
+            yield_header = ["Process"] + [category_inst.label for category_inst in category_insts]
+
+
 
             for process_inst, h in hists.items():
                 row = []
                 row.append(process_inst.label)
 
-                for i in range(h.axes["category"].size):
-                    if len(yield_header) <= h.axes["category"].size:
-                        yield_header.append(self.config_inst.get_category(h.axes["category"].bin(i)).label)
-                    row.append(f"{round(h[i].value)} $\pm$ {round(h[i].variance)}")
+                for category_inst in category_insts:
+                    leaf_category_insts = category_inst.get_leaf_categories() or [category_inst]
+
+                    h_cat = h[{"category": [
+                        hist.loc(c.id)
+                        for c in leaf_category_insts
+                        if c.id in h.axes["category"]
+                    ]}]
+                    h_cat = h_cat[{"category": sum}]
+
+                    value = h_cat.value
+                    variance = h_cat.variance
+                    # TODO: rounding should be less arbitrary, e.g. always round to 4 significant
+                    row.append(
+                        f"{value:.1f}" if self.skip_variance else
+                        rf"${value:.1f} \pm {variance:.1f}$"
+                    )
 
                 yields.append(row)
 
             yield_table = tabulate(yields, headers=yield_header, tablefmt=self.table_format)
+            print(yield_table)
 
-            # TODO: create some output using something like
-            #
-            # self.output().dump(yield_table, formatter="txt")
-
-            with open(self.output().fn, "w") as f:
-                f.write(yield_table)
+            self.output().dump(yield_table)
