eedd06c0d8943034c7e45adf83baf864b0a9ea66
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2023-04-03 14:04:23 +0200

    started work on test suite for calibrators

---
+++ tests/test_calibrator.py
@@ -0,0 +1,16 @@
+__all__ = ["CalibratorUtilTest"]
+
+import unittest
+
+from columnflow.util import maybe_import
+from columnflow.calibration import util as calib_util
+
+np = maybe_import("numpy")
+ak = maybe_import("awkward")
+dak = maybe_import("dask_awkward")
+coffea = maybe_import("coffea")
+
+
+class CalibratorUtilTest(unittest.TestCase):
+    def __init__(self):
+        self.random = np.array([5.])
