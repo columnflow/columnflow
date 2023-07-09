d4c53690199b1090a7079f6ea4dbf9643999173e
Daniel Savoiu <daniel.savoiu@cern.ch>
2023-07-09 21:50:52 +0200

    Read inputs for lambda-valued variable expressions.

--- columnflow/tasks/histograms.py
+++ columnflow/tasks/histograms.py
@@ -117,12 +117,19 @@ class CreateHistograms(
         read_columns = {"category_ids", "process_id"} | set(aliases.values())
         read_columns |= {
             Route(variable_inst.expression)
-            if isinstance(variable_inst.expression, str) else
-            None  # TODO: handle variable_inst with custom expressions, can they declare columns?
             for variable_inst in (
                 self.config_inst.get_variable(var_name)
                 for var_name in law.util.flatten(self.variable_tuples.values())
             )
+            if isinstance(variable_inst.expression, str)
+        } | {
+            # for variable_inst with custom expressions, read columns declared via aux key
+            Route(inp)
+            for variable_inst in (
+                self.config_inst.get_variable(var_name)
+                for var_name in law.util.flatten(self.variable_tuples.values())
+            )
+            for inp in variable_inst.x("inputs", [])
         }
         if self.dataset_inst.is_mc:
             read_columns |= {Route(column) for column in self.config_inst.x.event_weights}
