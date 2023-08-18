0acff8234cabe9f106c16fb90d072ca00f712658
Mathis Frahm <mathisfrahm@gmx.de>
2023-08-18 15:47:09 +0200

    add customizabilty for outlier mode/message

--- columnflow/production/cms/pdf.py
+++ columnflow/production/cms/pdf.py
@@ -30,17 +30,42 @@ set_ak_column_f32 = functools.partial(set_ak_column, value_type=np.float32)
     # only run on mc
     mc_only=True,
 )
-def pdf_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
+def pdf_weights(
+    self: Producer,
+    events: ak.Array,
+    outlier_threshold: float = 0.5,
+    outlier_mode: str = "keep",
+    outlier_message_type: str = "warning",
+    **kwargs,
+) -> ak.Array:
     """
     Producer that determines the pdf up and down variations on an event-by-event basis.
     This producer assumes that the nominal entry is always the first LHEPdfWeight value and
     that the nominal weight is already included in the LHEWeight.
     Can only be called with MC datasets.
 
+    The *outlier_mode* defines the prodcedure of how to handle events with a pdf uncertainty above
+    the *outlier_threshold*. Supported modes are:
+
+        - ``"keep"``: events are kept unmodified
+        - ``"remove"``: pdf weight nominal/up/down are all set to 0, essentially removing the event
+
+    The *outlier_message_type* allows to define the type of message that is given for datasets with outliers:
+
+        - ``"ignore"``: no message is given
+        - ``"info"``: a `logger.info` message is given
+        - ``"debug"``: a `logger.debug` message is given
+        - ``"warning"``: a `logger.warning` message is given
+        - ``"raise"``: an Exception is raised
+
     Resources:
 
        - https://arxiv.org/pdf/1510.03865.pdf
     """
+
+    assert outlier_mode in ("keep", "remove")
+    assert outlier_message_type in ("ignore", "info", "debug", "warning", "raise")
+
     # check for the correct amount of weights
     n_weights = ak.num(events.LHEPdfWeight, axis=1)
     bad_mask = (n_weights != 101) & (n_weights != 103)
@@ -74,14 +99,35 @@ def pdf_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     events = set_ak_column_f32(events, "pdf_weight_up", 1 + stddev)
     events = set_ak_column_f32(events, "pdf_weight_down", 1 - stddev)
 
-    if ak.any(outlier_mask := (stddev > 0.5)):
+    if ak.any(outlier_mask := (stddev > outlier_threshold)):
         # catch events with large pdf variations
         occurances = ak.sum(outlier_mask)
         frac = occurances / ak.count(stddev, axis=0) * 100
-        logger.warning(
+        msg = (
             f"In dataset {self.dataset_inst.name}, there are {occurances} ({frac:.2f}%) "
-            "entries with pdf uncertainty above 50%",
+            "entries with pdf uncertainty above 50%"
         )
+
+        if outlier_mode == "remove":
+            # set all pdf weights to 0 when the *outlier_threshold* is passed
+            events = set_ak_column_f32(events, "pdf_weight", ak.where(outlier_mask, 0, events.pdf_weight))
+            events = set_ak_column_f32(events, "pdf_weight_up", ak.where(outlier_mask, 0, events.pdf_weight_up))
+            events = set_ak_column_f32(events, "pdf_weight_down", ak.where(outlier_mask, 0, events.pdf_weight_down))
+
+            msg += ". The nominal/up/down pdf_weight columns have been set to 0 for these events."
+
+        if outlier_message_type == "raise":
+            raise Exception(msg)
+
+        msg_func = {
+            "ignore": None,
+            "info": logger.info,
+            "warning": logger.warning,
+            "debug": logger.debug,
+            "raise": None,
+        }[outlier_message_type]
+        if msg_func:
+            msg_func(msg)
 
     if ak.any(invalid_pdf_weight := (pdf_weight_nominal == 0)):
         # set all pdf weights to 0 when the nominal pdf weight is 0
