ab5b398949188a9ada55f55cb48df27f49c32eb7
Anas Haddad <hhhaaanas@gmail.com>
2023-11-06 17:53:03 +0100

    fixed imports

--- columnflow/tasks/ml.py
+++ columnflow/tasks/ml.py
@@ -27,7 +27,7 @@ from columnflow.tasks.framework.decorators import view_output_plots
 from columnflow.tasks.reduction import MergeReducedEventsUser, MergeReducedEvents
 from columnflow.tasks.production import ProduceColumns
 from columnflow.util import dev_sandbox, safe_div, DotDict
-from modules.columnflow.columnflow.util import maybe_import
+from columnflow.util import maybe_import
 
 
 ak = maybe_import("awkward")
