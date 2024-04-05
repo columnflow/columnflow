6ba299bdc5b81219fdd32bac99b2687f263352e7
juvanden <jules.vandenbroeck@cern.ch>
2024-04-05 09:22:57 +0200

    name changes to folders

---
+++ analysis_templates/ghent_template/__cf_module_name__/analysis/__cf_short_name_lc__.py
@@ -0,0 +1,8 @@
+
+"""
+Main analysis object for the __cf_short_name_lc__ analysis
+"""
+
+from __cf_short_name_lc__.analysis.create_analysis import create_analysis
+
+__cf_short_name_lc__ = create_analysis("__cf_short_name_lc__", 3, tags={"is_signal_region"})
