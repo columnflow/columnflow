1105e9fc2f2cdb8a2c143d5629dd33ed368a0e66
Mathis Frahm <mathisfrahm@gmx.de>
2024-06-13 15:55:25 +0200

    allow stitching in normalization_weights Producer

--- columnflow/production/normalization.py
+++ columnflow/production/normalization.py
@@ -4,8 +4,11 @@
 Column production methods related to sample normalization event weights.
 """
 
+from collections import defaultdict
+
+import law
+
 from columnflow.production import Producer, producer
-from columnflow.production.processes import process_ids
 from columnflow.util import maybe_import, InsertableDict
 from columnflow.columnar_util import set_ak_column
 
@@ -16,8 +19,8 @@ ak = maybe_import("awkward")
 
 
 @producer(
-    uses={process_ids, "mc_weight"},
-    produces={process_ids, "normalization_weight"},
+    uses={"process_id", "mc_weight"},
+    produces={"normalization_weight"},
     # only run on mc
     mc_only=True,
 )
@@ -25,11 +28,8 @@ def normalization_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Arra
     """
     Uses luminosity information of internal py:attr:`config_inst`, the cross section of a process
     obtained through :py:class:`category_ids` and the sum of event weights from the
-    py:attr:`selection_stats` attribute to assign each event a normalization weight.
+    py:attr:`normalization_selection_stats` attribute to assign each event a normalization weight.
     """
-    # add process ids
-    events = self[process_ids](events, **kwargs)
-
     # get the lumi
     lumi = self.config_inst.x.luminosity.nominal
 
@@ -56,13 +56,31 @@ def normalization_weights_requires(self: Producer, reqs: dict) -> None:
     # TODO: for actual sample stitching, we don't need the selection stats for that dataset, but
     #       rather the one merged for either all datasets, or the "stitching group"
     #       (i.e. all datasets that might contain any of the sub processes found in a dataset)
+
+    if self.dataset_inst.x("stitching_datasets", None):
+        stats_datasets = law.util.make_list(self.dataset_inst.x.stitching_datasets)
+    else:
+        stats_datasets = [self.dataset_inst.name]
+
+    # check that all datasets are known
+    for dataset in stats_datasets:
+        if not self.config_inst.has_dataset(dataset):
+            raise Exception(
+                f"unknown dataset '{dataset}' required for normalization weights computation",
+            )
+
     from columnflow.tasks.selection import MergeSelectionStats
-    reqs["selection_stats"] = MergeSelectionStats.req(
-        self.task,
-        tree_index=0,
-        branch=-1,
-        _exclude=MergeSelectionStats.exclude_params_forest_merge,
-    )
+    reqs["normalization_selection_stats"] = {
+        dataset: MergeSelectionStats.req(
+            self.task,
+            dataset=dataset,
+            tree_index=0,
+            branch=-1,
+            _exclude=MergeSelectionStats.exclude_params_forest_merge,
+        )
+        for dataset in stats_datasets
+    }
+    return reqs
 
 
 @normalization_weights.setup
@@ -76,14 +94,26 @@ def normalization_weights_setup(
     Sets up objects required by the computation of normalization weights and stores them as instance
     attributes:
 
-        - py:attr:`selection_stats`: The stats dict loaded from the output of MergeSelectionsStats.
+        - py:attr:`normalization_selection_stats`: The stats dict loaded from the output of MergeSelectionsStats.
         - py:attr:`sum_weights_table`: A sparse array serving as a lookup table for the sum of event
           weights per process id.
         - py:attr:`xs_table`: A sparse array serving as a lookup table for cross sections of all
           processes known to the config of the task, with keys being process ids.
     """
     # load the selection stats
-    selection_stats = inputs["selection_stats"]["collection"][0]["stats"].load(formatter="json")
+    normalization_selection_stats = [
+        inp["collection"][0]["stats"].load(formatter="json")
+        for inp in inputs["normalization_selection_stats"].values()
+    ]
+
+    if len(normalization_selection_stats) > 1:
+        # if necessary, merge the selection stats
+        from columnflow.tasks.selection import MergeSelectionStats
+        merged_selection_stats = defaultdict(float)
+        for stats in normalization_selection_stats:
+            MergeSelectionStats.merge_counts(merged_selection_stats, stats)
+    else:
+        merged_selection_stats = normalization_selection_stats[0]
 
     # for the lookup tables below, determine the maximum process id
     process_insts = [
@@ -95,7 +125,7 @@ def normalization_weights_setup(
 
     # ensure that the selection stats do not contain any process that was not previously registered
     unregistered_process_ids = [
-        int(process_id) for process_id in selection_stats["sum_mc_weight_per_process"]
+        int(process_id) for process_id in merged_selection_stats["sum_mc_weight_per_process"]
         if int(process_id) > max_id
     ]
     if unregistered_process_ids:
@@ -107,7 +137,7 @@ def normalization_weights_setup(
 
     # create a event weight sum lookup table with all known processes
     sum_weights_table = sp.sparse.lil_matrix((1, max_id + 1), dtype=np.float32)
-    for process_id, sum_weights in selection_stats["sum_mc_weight_per_process"].items():
+    for process_id, sum_weights in merged_selection_stats["sum_mc_weight_per_process"].items():
         sum_weights_table[0, int(process_id)] = sum_weights
     self.sum_weights_table = sum_weights_table
 
