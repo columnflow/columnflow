0ebae1f6527b4abc2b9dc770ff6415c5079954a0
Marcel R. <github.riga@icloud.com>
2024-07-17 13:18:57 +0200

    Use --root-prefix in micromamba setup.

--- setup.sh
+++ setup.sh
@@ -627,7 +627,7 @@ cf_setup_software_stack() {
 
                 mkdir -p "${CF_CONDA_BASE}/etc/profile.d"
                 curl -Ls "https://micro.mamba.pm/api/micromamba/${conda_arch}/latest" | tar -xvj -C "${CF_CONDA_BASE}" "bin/micromamba" > /dev/null
-                2>&1 "${CF_CONDA_BASE}/bin/micromamba" --prefix "$PWD" shell hook -y &> micromamba.sh
+                2>&1 "${CF_CONDA_BASE}/bin/micromamba" shell hook -y --root-prefix "$PWD" &> micromamba.sh
                 ret="$?"
                 if [ "${ret}" != "0" ]; then
                     [ -f "micromamba.sh" ] && >&2 cat micromamba.sh
