e8c94891a93f024d40784f4e27f43750add5bdf8
Balduin <balduin.letzer@studium.uni-hamburg.de>
2024-08-12 16:05:54 +0200

    efficiency and twin hist draw method

--- columnflow/plotting/plot_all.py
+++ columnflow/plotting/plot_all.py
@@ -5,6 +5,7 @@ Example plot function.
 """
 
 from __future__ import annotations
+import law
 
 from columnflow.types import Sequence
 from columnflow.util import maybe_import, try_float
@@ -16,6 +17,8 @@ mpl = maybe_import("matplotlib")
 plt = maybe_import("matplotlib.pyplot")
 mplhep = maybe_import("mplhep")
 od = maybe_import("order")
+
+logger = law.logger.get_logger(__name__)
 
 
 def draw_error_bands(
@@ -154,6 +157,76 @@ def draw_errorbars(
     ax.errorbar(**defaults)
 
 
+def binom_int(num, den, confint=0.68):
+    from scipy.stats import beta
+    quant = (1 - confint) / 2.
+    low = beta.ppf(quant, num, den - num + 1)
+    high = beta.ppf(1 - quant, num + 1, den - num)
+    return (np.nan_to_num(low), np.where(np.isnan(high), 1, high))
+
+
+def draw_efficiency(
+    ax: plt.Axes,
+    h: hist.Hist,
+    norm: float | Sequence | np.ndarray = 1.0,
+    **kwargs,
+) -> None:
+
+    # Extract histogram data
+    values = h.values()
+    bins = h.axes[0].edges
+
+    efficiency = np.nan_to_num(values / norm)
+
+    if np.any(efficiency > 1):
+        logger.warning(
+            f"Some efficiencies for {kwargs['label']} are greater than 1, errorbars are capped at zero"
+            )
+    elif np.any(efficiency < 0):
+        logger.warning(
+            f"Some efficiencies for {kwargs['label']} are less than 0, errorbars are capped at zero"
+            )
+
+    # getting error bars
+    band_low, band_high = binom_int(values, norm)
+    error_low = np.asarray(efficiency - band_low)
+    error_high = np.asarray(band_high - efficiency)
+
+    # removing large errors in empty bins
+    error_low[error_low == 1] = 0
+    error_high[error_high == 1] = 0
+
+    # removing negative errors
+    error_low[error_low < 0] = 0
+    error_high[error_high < 0] = 0
+
+    # stacking errors
+    errors = np.concatenate(
+        (error_low.reshape(error_low.shape[0], 1), error_high.reshape(error_high.shape[0], 1)),
+        axis=1,
+    )
+    errors = errors.T
+
+    ax.errorbar(x=bins[:-1], y=efficiency, yerr=errors, fmt="o-", label=kwargs["label"])
+
+    ax.set_xlabel("Bin")
+    ax.set_ylabel("Counts")
+    ax.set_ylim(0, 1)
+    ax.legend()
+
+
+def draw_hist_twin(
+    ax: plt.Axes,
+    h: hist.Hist,
+    norm: float | Sequence | np.ndarray = 1.0,
+    **kwargs,
+) -> None:
+    ax2 = ax.twinx()
+    draw_hist(ax2, h, norm, **kwargs)
+    bin_widths = h.axes[0].widths
+    ax2.set_ylabel(r"Entries / {:.2f} GeV".format(bin_widths[0]))
+
+
 def plot_all(
     plot_config: dict,
     style_config: dict,
@@ -195,7 +268,10 @@ def plot_all(
     # available plot methods mapped to their names
     plot_methods = {
         func.__name__: func
-        for func in [draw_error_bands, draw_stack, draw_hist, draw_profile, draw_errorbars]
+        for func in [
+            draw_error_bands, draw_stack, draw_hist, draw_profile, draw_hist_twin,
+            draw_errorbars, draw_efficiency,
+        ]
     }
 
     plt.style.use(mplhep.style.CMS)
