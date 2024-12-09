168ad0b109ea69cdb465654bcee519420dcb6ede
maadcoen <maarten.de.coen@cern.ch>
2024-12-09 16:16:25 +0100

    reorganize b-tagging

--- columnflow/tasks/cmsGhent/btagefficiency.py
+++ columnflow/tasks/cmsGhent/btagefficiency.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import hist
+
 import law
 import order as od
 from collections import OrderedDict
@@ -7,131 +9,39 @@ from itertools import product
 
 from columnflow.tasks.framework.base import Requirements
 from columnflow.tasks.framework.mixins import (
-    CalibratorsMixin, VariablesMixin,
-    DatasetsProcessesMixin, SelectorMixin,
+    CalibratorsMixin, VariablesMixin, SelectorMixin,
 )
 from columnflow.tasks.framework.plotting import (
     PlotBase, PlotBase2D,
 )
-
-from columnflow.tasks.selection import MergeSelectionStats
+from columnflow.tasks.cmsGhent.selection_hists import SelectionEfficiencyHistMixin
 
 from columnflow.tasks.framework.remote import RemoteWorkflow
-from columnflow.util import dev_sandbox, dict_add_strict
-from columnflow.types import Any
+from columnflow.util import dev_sandbox, dict_add_strict, DotDict
+
+
+class BTagEfficiencyBase:
+    tag_name = "btag"
+    flav_name = "hadronFlavour"
+    flavours = {0: "light", 4: "charm", 5: "bottom"}
+    wps = ["L", "M", "T"]
+
+    sandbox = dev_sandbox(law.config.get("analysis", "default_columnar_sandbox"))
 
 
 class BTagEfficiency(
+    BTagEfficiencyBase,
+    SelectionEfficiencyHistMixin,
     VariablesMixin,
-    DatasetsProcessesMixin,
     SelectorMixin,
     CalibratorsMixin,
     law.LocalWorkflow,
     RemoteWorkflow,
-    PlotBase2D,
 ):
-
-    plot_function = PlotBase.plot_function.copy(
-        default="columnflow.plotting.plot_functions_2d.plot_2d",
-        add_default_to_description=True,
-    )
-
-    sandbox = dev_sandbox(law.config.get("analysis", "default_columnar_sandbox"))
-
-    # upstream requirements
-    reqs = Requirements(
-        RemoteWorkflow.reqs,
-        MergeSelectionStats=MergeSelectionStats,
-    )
-
-    @classmethod
-    def resolve_param_values(
-            cls,
-            params: law.util.InsertableDict[str, Any],
-    ) -> law.util.InsertableDict[str, Any]:
-        """
-        Resolve values *params* and check against possible default values
-
-        Check the values in *params* against the default value ``"default_btagAlgorithm"`` in the current config inst.
-        For more information, see
-        :py:meth:`~columnflow.tasks.framework.base.ConfigTask.resolve_config_default_and_groups`.
-
-        :param params: Parameter values to resolve
-        :return: Dictionary of parameters that contains the list requested
-            :py:class:`~columnflow.calibration.Calibrator` instances under the
-            keyword ``"calibrator_insts"``. See :py:meth:`~.CalibratorsMixin.get_calibrator_insts`
-            for more information.
-        """
-        redo_default_variables = False
-        if "variables" in params:
-            # when empty, use the config default
-            if not params["variables"]:
-                redo_default_variables = True
-
-        params = super().resolve_param_values(params)
-
-        config_inst = params.get("config_inst")
-        if not config_inst:
-            return params
-
-        if redo_default_variables:
-            # when empty, use the config default
-            if config_inst.x("default_btag_variables", ()):
-                params["variables"] = tuple(config_inst.x.default_btag_variables)
-            elif cls.default_variables:
-                params["variables"] = tuple(cls.default_variables)
-            else:
-                raise AssertionError(f"define default btag variables in {cls.__class__} or config {config_inst.name}")
-
-        return params
-
-    def workflow_requires(self):
-        reqs = super().workflow_requires()
-        for d in self.datasets:
-            reqs[d] = self.reqs.MergeSelectionStats.req(
-                self,
-                tree_index=0,
-                branch=-1,
-                dataset=d,
-                _exclude=MergeSelectionStats.exclude_params_forest_merge,
-            )
-        return reqs
-
-    def requires(self):
-        return {
-            d: self.reqs.MergeSelectionStats.req(
-                self,
-                tree_index=0,
-                branch=-1,
-                dataset=d,
-                _exclude=MergeSelectionStats.exclude_params_forest_merge,
-            )
-            for d in self.datasets
-        }
-
-    def create_branch_map(self):
-        # create a dummy branch map so that this task could be submitted as a job
-        return {0: None}
-
-    def store_parts(self):
-        parts = super().store_parts()
-        parts.insert_before("version", "datasets", f"datasets_{self.datasets_repr}")
-        return parts
-
     def output(self):
         return {
-            "stats": self.target(".".join(
-                self.get_plot_names("btagging_efficiency")[0].split(".")[:-1],
-            ) + ".json"),
-            "plots": [
-                [self.target(name)
-                 for name in self.get_plot_names(
-                    f"btag_eff__{flav}_hadronflavour"
-                    f"__wp_{wp}",
-                )]
-                for flav in ["udsg", "c", "b"]
-                for wp in ["L", "M", "T"]
-            ],
+            "json": self.target(f"{self.tag_name}_efficiency.json"),
+            "hist": self.target(f"{self.tag_name}_efficiency.pickle"),
         }
 
     def get_plot_parameters(self):
@@ -140,86 +50,124 @@ class BTagEfficiency(
         dict_add_strict(params, "legend_title", "Processes")
         return params
 
+
     @law.decorator.log
     def run(self):
         import hist
-        import numpy as np
         import correctionlib
         import correctionlib.convert
         from columnflow.plotting.cmsGhent.plot_util import cumulate
 
         variable_insts = list(map(self.config_inst.get_variable, self.variables))
-
-        # histogram for the tagged and all jets (combine all datasets)
-        histogram = 0
-        for dataset, inp in self.input().items():
-            dataset_inst = self.config_inst.get_dataset(dataset)
-            dt_process_insts = {process_inst for process_inst, _, _ in dataset_inst.walk_processes()}
-            xsec = sum(
-                process_inst.get_xsec(self.config_inst.campaign.ecm).nominal
-                for process_inst in dt_process_insts
-            )
-            h_in = inp["collection"][0]["hists"].load(formatter="pickle")["btag_efficiencies"]
-            histogram = histogram + h_in * xsec / inp["collection"][0]["stats"].load()["sum_mc_weight"]
-
-        if not histogram:
-            raise Exception(
-                "no histograms found to plot; possible reasons:\n" +
-                "  - requested variable requires columns that were missing during histogramming\n" +
-                "  - selected --processes did not match any value on the process axis of the input histogram",
-            )
+        histograms = self.read_hist(variable_insts)
+        sum_histogram = sum(histograms.values())
 
         # combine tagged and inclusive histograms to an efficiency histogram
-        cum_histogram = cumulate(histogram, direction="above", axis="btag_wp")
-        incl = cum_histogram[{"btag_wp": slice(0, 1)}].values()
+        cum_histogram = cumulate(sum_histogram, direction="above", axis=f"{self.tag_name}_wp")
+        incl = cum_histogram[{f"{self.tag_name}_wp": slice(0, 1)}].values()
 
         axes = OrderedDict(zip(cum_histogram.axes.name, cum_histogram.axes))
-        axes["btag_wp"] = hist.axis.StrCategory(["L", "M", "T"], name="btag_wp", label="working point")
+        axes[f"{self.tag_name}_wp"] = hist.axis.StrCategory(self.wps, name=f"{self.tag_name}_wp", label="working point")
 
-        efficiency_hist = hist.Hist(*axes.values(), name=histogram.name, storage=hist.storage.Weight())
-        efficiency_hist.view()[:] = cum_histogram[{"btag_wp": slice(1, None)}].view()
-        efficiency_hist = efficiency_hist / incl
+        selected_counts = hist.Hist(*axes.values(), name=sum_histogram.name, storage=hist.storage.Weight())
+        selected_counts.view()[:] = cum_histogram[{"btag_wp": slice(1, None)}].view()
+
+        efficiency_hist = self.efficiency(selected_counts, incl)
+
+        # save as pickle hist
+        self.output()["hist"].dump(efficiency_hist, formatter="pickle")
 
         # save as correctionlib file
         efficiency_hist.label = "out"
-        description = f"b-tagging efficiencies of jets for {efficiency_hist.name} algorithm"
-        clibcorr = correctionlib.convert.from_histogram(efficiency_hist)
+        description = f"{self.tag_name} efficiencies of jets for {efficiency_hist.name} algorithm"
+        clibcorr = correctionlib.convert.from_histogram(efficiency_hist[{"systematic": "central"}])
         clibcorr.description = description
 
         cset = correctionlib.schemav2.CorrectionSet(schema_version=2, description=description, corrections=[clibcorr])
-        self.output()["stats"].dump(cset.dict(exclude_unset=True), indent=4, formatter="json")
-        # plot efficiency for each hadronFlavour and wp
-        for i, (hadronFlavour, wp) in enumerate(product([0, 4, 5], "LMT")):
+        self.output()["json"].dump(cset.dict(exclude_unset=True), indent=4, formatter="json")
 
+        if not self.make_plots:
+            return
+
+
+
+class BTagEfficiencyPlot(
+    BTagEfficiencyBase,
+    PlotBase2D,
+):
+    reqs = Requirements(BTagEfficiency=BTagEfficiency)
+
+    def create_branch_map(self):
+        return [
+            DotDict({"flav": flav, "wp": wp})
+            for flav in self.flavours.values()
+            for wp in self.wps
+        ]
+
+    def requires(self):
+        return {
+            d: self.reqs.MergeHistograms.req(
+                self,
+                dataset=d,
+                branch=-1,
+                _exclude={"branches"},
+            )
+            for d in self.datasets
+        }
+
+    def output(self):
+        return [
+            [
+                self.target(name)
+                for name in self.get_plot_names(
+                    f"{self.tag_name}_eff__{self.branch_data.wp}_{self.flav_name}"
+                    f"__wp_{self.branch_data.wp}" +
+                    (f"__err_{dr}" if dr != "central" else ""),
+                )
+            ]
+            for dr in ["central", "down", "up"]
+        ]
+
+    def run(self):
+        import numpy as np
+
+        variable_insts = list(map(self.config_inst.get_variable, self.variables))
+
+        # plot efficiency for each hadronFlavour and wp
+        efficiency_hist = self.input()["hist"].load(formatter="pickle")
+
+        for i, sys in enumerate(["central", "down", "up"]):
             # create a dummy histogram dict for plotting with the first process
             # TODO change process name to the relevant process group
-            hist_dict = OrderedDict((
-                (self.config_inst.get_process(self.processes[-1]),
-                 efficiency_hist[{
-                     "hadronFlavour": hist.loc(hadronFlavour),
-                     "btag_wp": wp,
-                 }]),),
-            )
+            h = efficiency_hist[{
+                 self.flav_name: hist.loc(self.branch_data.flav),
+                 f"{self.tag_name}_wp": self.branch_data.wp,
+            }]
+
+            h_sys = h[{"systematics": sys}]
+            if sys != "central":
+                h_sys -= h[{"systematics": "central"}].values()
+
+            proc = self.config_inst.get_process(self.processes[-1])
 
             # create a dummy category for plotting
             cat = od.Category(
-                name="hadronFlavour",
-                label={0: "udsg flavour", 4: "charm flavour", 5: "bottom flavour"}[hadronFlavour],
+                name=self.flav_name,
+                label=self.flavours[self.branch_data.flav],
             )
 
             # custom styling:
-            label_values = np.around(
-                efficiency_hist[{"hadronFlavour": hist.loc(hadronFlavour)}].values() * 100, decimals=1)
+            label_values = np.round(h_sys.values() * 100, decimals=1)
             style_config = {"plot2d_cfg": {"cmap": "PiYG", "labels": label_values}}
             # call the plot function
             fig, _ = self.call_plot_func(
                 self.plot_function,
-                hists=hist_dict,
+                hists={proc: h_sys},
                 config_inst=self.config_inst,
                 category_inst=cat.copy_shallow(),
                 variable_insts=[var_inst.copy_shallow() for var_inst in variable_insts],
                 style_config=style_config,
                 **self.get_plot_parameters(),
             )
-            for p in self.output()["plots"][i]:
+            for p in self.output()[i]:
                 p.dump(fig, formatter="mpl")
---
+++ columnflow/tasks/cmsGhent/selection_hists.py
@@ -0,0 +1,271 @@
+from __future__ import annotations
+
+import luigi
+import law
+import order as od
+import hist
+
+from columnflow.tasks.framework.base import Requirements
+from columnflow.tasks.framework.mixins import DatasetsProcessesMixin
+from columnflow.tasks.selection import MergeSelectionStats
+from columnflow.tasks.framework.remote import RemoteWorkflow
+from columnflow.util import dev_sandbox
+
+from columnflow.tasks.framework.plotting import (
+    PlotBase, PlotBase1D, VariablePlotSettingMixin, ProcessPlotSettingMixin
+)
+
+from columnflow.types import Any
+
+
+class SelectionEfficiencyHistMixin(DatasetsProcessesMixin):
+
+    tag_name = "tag"
+    exclude_index = True
+
+    # upstream requirements
+    reqs = Requirements(
+        RemoteWorkflow.reqs,
+        MergeSelectionStats=MergeSelectionStats,
+    )
+
+    @classmethod
+    def get_default_variables(cls, cfg: od.Config):
+        return cfg.x(f"default_{cls.tag_name}_variables", tuple())
+
+    @classmethod
+    def resolve_param_values(
+            cls,
+            params: law.util.InsertableDict[str, Any],
+    ) -> law.util.InsertableDict[str, Any]:
+        f"""
+        Resolve values *params* and check against possible default values
+
+        Check the values in *params* against the default value in the current config inst. 
+        See {cls.__class__}.get_default_variables for where the default value should be stored
+        For more information, see
+        :py:meth:`~columnflow.tasks.framework.base.ConfigTask.resolve_config_default_and_groups`.
+        """
+        redo_default_variables = False
+        # when empty, use the config default
+        if not params.get("variables", None):
+            redo_default_variables = True
+
+        params = super().resolve_param_values(params)
+
+        config_inst = params.get("config_inst")
+        if not config_inst:
+            return params
+
+        if redo_default_variables:
+            # when empty, use the config default
+            if default_variables := cls.get_default_variables(config_inst):
+                params["variables"] = tuple(default_variables)
+            elif cls.default_variables:
+                params["variables"] = tuple(cls.default_variables)
+            else:
+                raise AssertionError(f"define default {cls.tag_name} variables "
+                                     f"in {cls.__class__} or config {config_inst.name}")
+
+        return params
+
+    def workflow_requires(self):
+        reqs = super().workflow_requires()
+        for d in self.datasets:
+            reqs[d] = self.reqs.MergeSelectionStats.req(
+                self,
+                tree_index=0,
+                branch=-1,
+                dataset=d,
+                _exclude=MergeSelectionStats.exclude_params_forest_merge,
+            )
+        return reqs
+
+    def requires(self):
+        return {
+            d: self.reqs.MergeSelectionStats.req(
+                self,
+                tree_index=0,
+                branch=-1,
+                dataset=d,
+                _exclude=MergeSelectionStats.exclude_params_forest_merge,
+            )
+            for d in self.datasets
+        }
+
+    def create_branch_map(self):
+        # create a dummy branch map so that this task could be submitted as a job
+        return {0: None}
+
+    def store_parts(self):
+        parts = super().store_parts()
+        parts.insert_before("version", "datasets", f"datasets_{self.datasets_repr}")
+        return parts
+
+    @classmethod
+    def get_process_xsec(cls, dataset: od.Dataset | str, config_inst: od.Config) -> tuple[od.Process, float]:
+        """
+        link dataset to a process and a xsec. Implemented as class method so it can be easily adapted to the
+        the needs of a specific analysis using a patch.
+        """
+        dataset_inst = dataset if isinstance(dataset, od.Dataset) else config_inst.get_dataset(dataset)
+        process_inst = dataset_inst.process.get_first()
+        if config_inst.has_process(process_inst):
+            process_inst = config_inst.get_process(process_inst)
+        xsec = 1 if process_inst.is_data else process_inst.get_xsec(config_inst.campaign.ecm).nominal
+        return process_inst, xsec
+
+    def read_hist(self, variable_insts=tuple(), name=None) -> dict[od.Process, hist.Hist]:
+        """
+        read the histograms calculated in MergeSelectionStats task, properly normalize,
+        and apply flow and merged bin settings from the given variables.
+        """
+        import numpy as np
+        from columnflow.plotting.plot_util import use_flow_bins
+
+        if name is None:
+            name = f"{self.tag_name}_efficiencies"
+        # histogram for the tagged and all jets (combine all datasets)
+
+        process_insts = list(map(self.config_inst.get_process, self.processes))
+        histograms = {}
+        for dataset, inp in self.input().items():
+            # map dataset to one of to requested processes
+            dataset_process, xsec = self.get_process_xsec(dataset, self.config_inst)
+            processes = [p for p in process_insts if p.has_process(dataset_process)]
+            assert len(processes) == 1, \
+                f"process {dataset_process.name} of dataset {dataset} in multiple processes {processes}"
+            process = processes[0]
+
+            h_in = inp["collection"][0]["hists"].load(formatter="pickle")[name]
+            for variable_inst in variable_insts:
+                v_idx = h_in.axes.name.index(variable_inst.name)
+                for mi, mj in variable_inst.x("merge_bins", []):
+                    merged_bins = h_in[{variable_inst.name: slice(mi, mj + 1, sum)}]
+                    new_arr = np.moveaxis(h_in.view(), v_idx, 0)
+                    new_arr[mi:mj + 1] = merged_bins.view()[np.newaxis]
+                    h_in.view()[:] = np.moveaxis(new_arr, 0, v_idx)
+
+                if any(flows := [
+                    getattr(variable_inst, f + "flow", variable_inst.x(f + "flow", False))
+                    for f in ["under", "over"]
+                ]):
+                    h_in = use_flow_bins(h_in, variable_inst.name, *flows)
+
+            if process.is_mc:
+                norm = inp["collection"][0]["stats"].load()["sum_mc_weight"]
+                h_in = h_in * xsec / norm * self.config_inst.x.luminosity.nominal
+
+            histograms[process] = histograms.get(process, 0) + h_in
+
+        if not histograms:
+            raise Exception(
+                "no histograms found to plot; possible reasons:\n" +
+                "  - requested variable requires columns that were missing during histogramming\n" +
+                "  - selected --processes did not match any value on the process axis of the input histogram",
+            )
+
+        return histograms
+
+    @classmethod
+    def efficiency(cls, selected_counts: hist.Hist, incl: hist.Hist, **kwargs) -> hist.Hist:
+        """
+        calculate efficiencies with uncertainties given two histograms **selected_counts** and **incl**
+        containing  the counts before and after selecton. The uncertainties are calculated using the method
+        **proportion_confint**  from the **statsmodels.stats.proportion** modul. The default output are
+        two-sided 65% level confidence intervals according to the Clopper-Pearson (beta) method, but other settings
+        can be chosen through the keyword arguments. For more documentation see
+
+        https://www.statsmodels.org/dev/generated/statsmodels.stats.proportion.proportion_confint.html
+
+        @param selected_counts: histogram with event counts after selection
+        @param incl: histogram with event counts before selection
+        @param kwargs: keyword arguments passed to **proportion_confint**
+        """
+        from statsmodels.stats.proportion import proportion_confint
+        efficiency = selected_counts / incl.values()
+        eff_sample_size_corr = incl.values() / incl.variances()
+        eff_selected = selected_counts.values() * eff_sample_size_corr
+        eff_incl = incl.values() * eff_sample_size_corr
+        kwargs = dict(alpha=0.35, method="beta") | kwargs
+        error = proportion_confint(eff_selected, eff_incl, **kwargs)
+        eff_full = hist.Hist(
+            hist.axis.StrCategory(["central", "down", "up"], name="systematic"),
+            *selected_counts.axes,
+            storage=hist.storage.Weight()
+        )
+        eff_full.values()[:] = [efficiency.values(), *error]
+        return eff_full
+
+
+class SelectionHistPlot(
+    SelectionEfficiencyHistMixin,
+    VariablePlotSettingMixin,
+    ProcessPlotSettingMixin,
+    PlotBase1D,
+):
+    exclude_index = False
+
+    hist_name = luigi.Parameter(
+        description="name of the selection histograms to plot",
+        significant=True,
+    )
+    cat_label = luigi.Parameter(
+        default="",
+        description="category label for the plot",
+        significant=False,
+    )
+
+    plot_function = PlotBase.plot_function.copy(
+        default="columnflow.plotting.plot_functions_1d.plot_variable_per_process",
+        add_default_to_description=True,
+    )
+
+    sandbox = dev_sandbox(law.config.get("analysis", "default_columnar_sandbox"))
+
+    @law.workflow_property(setter=True, cache=True, empty_value=0)
+    def hist_variables(self):
+        # check if the merging stats are present
+        all_hists = []
+        for d in self.datasets:
+            hists = self.reqs.MergeSelectionStats.req(self, dataset=d).output()["hists"]
+            if not hists.exists():
+                return []
+            all_hists.append(hists.load(formatter="pickle")[self.hist_name])
+        if not all([h.axes == all_hists[0].axes for h in all_hists]):
+            return []
+        return all_hists[0].axes.name
+
+    @law.dynamic_workflow_condition
+    def workflow_condition(self):
+        # the workflow shape can be constructed as soon as the histogram variables are know
+        return self.hist_variables
+
+    @workflow_condition.output
+    def output(self):
+        return {
+            vr: [self.target(name) for name in self.get_plot_names(vr)]
+            for vr in self.hist_variables
+        }
+
+    @law.decorator.log
+    def run(self):
+        variable_insts = list(map(self.config_inst.get_variable, self.variables))
+        histograms = self.read_hist(variable_insts)
+
+        for variable_name in self.hist_variables:
+            variable_inst = self.config_inst.get_variable(
+                variable_name,
+                default=od.Variable(variable_name)
+            )
+
+            fig, _ = self.call_plot_func(
+                self.control_plot_function,
+                hists={process_inst: h.project(variable_name) for process_inst, h in histograms.items()},
+                config_inst=self.config_inst,
+                category_inst=od.Category(name=self.cat_label),
+                variable_insts=[variable_inst.copy_shallow()],
+                **self.get_control_plot_parameter(),
+            )
+            for p in self.output()[variable_name]:
+                p.dump(fig, formatter="mpl")
