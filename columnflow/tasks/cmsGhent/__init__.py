17f839298184c490e754c24d1510382ee9a59ccd
maadcoen <maarten.de.coen@cern.ch>
2024-12-11 10:31:47 +0100

    add cmsGhent tasks to law

---
+++ columnflow/tasks/cmsGhent/__init__.py
@@ -0,0 +1,5 @@
+import columnflow.tasks.cmsGhent.plotting
+import columnflow.tasks.cmsGhent.trigger_scale_factors
+import columnflow.tasks.cmsGhent.cutflow
+import columnflow.tasks.cmsGhent.btagefficiency
+import columnflow.tasks.cmsGhent.config
