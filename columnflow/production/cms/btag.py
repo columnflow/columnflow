61d493f7293816632587d4cee45d4e767f73fe5f
Jules Vandenbroeck <Jules.Vandenbroeck@cern.ch>
2025-01-21 09:46:25 +0100

    added sources feature to BTagSFConfig class

--- columnflow/production/cms/btag.py
+++ columnflow/production/cms/btag.py
@@ -24,6 +24,7 @@ logger = law.logger.get_logger(__name__)
 @dataclass
 class BTagSFConfig:
     correction_set: str
+    sources: list[str]
     jec_sources: list[str]
     discriminator: str = ""  # when empty, set in post init based on correction set
     corrector_kwargs: dict[str, Any] = field(default_factory=dict)
