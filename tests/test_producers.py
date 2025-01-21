44b9e30e17829b50e9be87e9066c0d266973eb15
Philip Keicher <philip.keicher@cern.ch>
2025-01-21 08:37:47 +0100

    tests for initialization

--- tests/test_producers.py
+++ tests/test_producers.py
@@ -1,15 +1,22 @@
 import unittest
 import os
-from pathlib import Path
+
+from itertools import chain, combinations
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
-from columnflow.util import maybe_import
-from columnflow.production.cms.supercluster_eta import photon_sceta, electron_sceta
+from columnflow.calibration.cms.egamma import EGammaCorrectionConfig, egamma_scale_corrector
+from columnflow.util import maybe_import, DotDict
+from columnflow.types import Any, Iterable
+from columnflow.production import Producer
 
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
 
+
+"""
+from columnflow.production.cms.supercluster_eta import photon_sceta, electron_sceta
+from pathlib import Path
 
 class SCEtaProducerTests(unittest.TestCase):
 
@@ -25,7 +32,8 @@ class SCEtaProducerTests(unittest.TestCase):
 
     def test_photon_sceta(self):
         foo = self.photon_producer(self.ref_data)
-
+        from IPython import embed
+        embed(header="test_photon_sceta")
         def cap_digits_in_list(in_list, n=2):
             round_lambda = lambda x: round(x, n)
             return [list(map(round_lambda, x)) for x in in_list]
@@ -33,4 +41,49 @@ class SCEtaProducerTests(unittest.TestCase):
         self.assertListEqual(
             cap_digits_in_list(foo.Photon.superclusterEta.to_list()),
             cap_digits_in_list(self.ref_data.Photon.superclusterEta.to_list()),
+        )"""
+
+
+class EGammaCalibratorTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+
+        self.necessary_keywords: dict[str, Any] = {
+            "source_field": "foo",
+            "get_correction_file": lambda x: "foo",
+            "get_scale_config": lambda x: EGammaCorrectionConfig(),
+            "dataset_inst": DotDict.wrap({"is_mc": False}),
+        }
+
+    def test_egamma_scale_corrector(self):
+        # make sure that the egamma scale corrector is abstract and
+        # doesn't work without the necessary properties
+
+        with self.assertRaises(TypeError):
+            egamma_scale_corrector()
+
+        def powerset(iterable: Iterable[Any]) -> Iterable[Any]:
+            """
+            powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+            """
+            s = list(iterable)
+            return chain.from_iterable(combinations(s, r) for r in range(len(s)))
+
+        for subset in powerset(self.necessary_keywords):
+            with self.subTest(subset=subset):
+                with self.assertRaises(TypeError):
+                    foo: Producer = egamma_scale_corrector.derive(
+                        "foo", cls_dict={
+                            x: self.necessary_keywords[x] for x in subset
+                        },
+                    )
+                    foo()
+
+        # test that the corrector works with the necessary properties
+        foo: Producer = egamma_scale_corrector.derive(
+            "foo", cls_dict=self.necessary_keywords,
         )
+        from IPython import embed
+        embed(header="test_egamma_scale_corrector")
+        foo()
