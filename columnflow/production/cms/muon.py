f33afcd7babb2e14d13f2386a1646065c34cc451
Marcel Rieger <riga@users.noreply.github.com>
2025-02-10 14:54:10 +0100

    Typo.

--- columnflow/production/cms/muon.py
+++ columnflow/production/cms/muon.py
@@ -10,7 +10,9 @@ from dataclasses import dataclass
 
 from columnflow.production import Producer, producer
 from columnflow.util import maybe_import, InsertableDict
-from columnflow.columnar_util import set_ak_column, flat_np_view, layout_ak_array
+from columnflow.columnar_util import (
+    set_ak_column, flat_np_view, layout_ak_array, load_correction_set,
+)
 
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
