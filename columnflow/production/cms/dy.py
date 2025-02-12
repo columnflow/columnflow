cf647ae269f9ec817fa3419173916ad2d825daaa
Ana A <ana.rita.alves.andrade@cern.ch>
2025-02-12 11:43:42 +0100

    redifining masks and adding dy_weights_init

--- columnflow/production/cms/dy.py
+++ columnflow/production/cms/dy.py
@@ -5,7 +5,6 @@ Column production methods related to Drell-Yan reweighting.
 """
 
 from __future__ import annotations
-import functools
 
 import law
 
@@ -18,11 +17,6 @@ from columnflow.columnar_util import set_ak_column
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
 vector = maybe_import("vector")
-
-
-# helper
-set_ak_column_f32 = functools.partial(set_ak_column, value_type=np.float32)
-
 
 logger = law.logger.get_logger(__name__)
 
@@ -42,7 +36,7 @@ class DrellYanConfig:
     uses={"GenPart.*"},
     produces={"gen_dilepton.pt"},
 )
-def get_gen_dilepton(self, events: ak.Array, **kwargs) -> ak.Array:
+def gen_dilepton(self, events: ak.Array, **kwargs) -> ak.Array:
     """
     Reconstruct the di-lepton pair from generator level info.
     """
@@ -52,21 +46,22 @@ def get_gen_dilepton(self, events: ak.Array, **kwargs) -> ak.Array:
     status = events.GenPart.status
 
     # electrons and muons need to have status == 1, so find them
-    ele_mu_mask = (pdg_id == 11) | (pdg_id == 13)
-    final_ele_mu_mask = ele_mu_mask & (status == 1)
+    ele_mu_mask = (
+        ((pdg_id == 11) | (pdg_id == 13)) &
+        (status == 1) &
+        events.GenPart.hasFlags("fromHardProcess")
+    )
 
     # taus need to have status == 2,
-    tau_mask = pdg_id == 15
-    final_tau_mask = tau_mask & (status == 2)
+    tau_mask = (
+        (pdg_id == 15) &
+        (status == 2) &
+        events.GenPart.hasFlags("fromHardProcess")
+    )
 
     # combine the masks
-    lepton_mask = final_ele_mu_mask | final_tau_mask
-    # only consider leptons from hard process (i.e. from the matrix element)
-    mask_gen = ak.mask(events.GenPart, lepton_mask).hasFlags("fromHardProcess")
-
-    # fill the mask with False if it is None and extract the gen leptons
-    mask_gen = ak.fill_none(mask_gen, False)
-    lepton_pairs = events.GenPart[mask_gen]
+    lepton_mask = ele_mu_mask | tau_mask
+    lepton_pairs = events.GenPart[lepton_mask]
 
     # some up the four momenta of the leptons
     lepton_pair_momenta = lepton_pairs.sum(axis=-1)
@@ -77,39 +72,42 @@ def get_gen_dilepton(self, events: ak.Array, **kwargs) -> ak.Array:
 
 
 @producer(
-    uses={get_gen_dilepton.PRODUCES},
+    uses={"gen_dilepton.pt"},
+    # weight variations are defined in init
     produces={"dy_weight"},
     # only run on mc
     mc_only=True,
     # function to determine the correction file
-    get_dy_file=(lambda self, external_files: external_files.dy_sf),
+    get_dy_weight_file=(lambda self, external_files: external_files.dy_weight_sf),
     # function to load the config
-    get_dy_config=(lambda self: self.config_inst.x.dy_config),
+    get_dy_weight_config=(lambda self: self.config_inst.x.dy_weight_config),
 )
 def dy_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     """
-    Creates Drell-Yan weights using the correctionlib. Requires an external file in the config under
-    ``dy_sf``:
+    Creates Drell-Yan weights using the correctionlib.
+    https://cms-higgs-leprare.docs.cern.ch/htt-common/DY_reweight/#correctionlib-file
+
+    Requires an external file in the config under ``dy_weight_sf``:
 
     .. code-block:: python
 
         cfg.x.external_files = DotDict.wrap({
-            "dy_sf": "/afs/cern.ch/work/m/mrieger/public/mirrors/external_files/DY_pTll_weights_v1.json.gz",  # noqa
+            "dy_weight_sf": "/afs/cern.ch/work/m/mrieger/public/mirrors/external_files/DY_pTll_weights_v1.json.gz",  # noqa
         })
 
-    *get_dy_file* can be adapted in a subclass in case it is stored differently in the external files.
+    *get_dy_weight_file* can be adapted in a subclass in case it is stored differently in the external files.
 
-    The campaign era and name of the correction set should be given as an auxiliary entry in the config:
+    The campaign era and name of the correction set (see link above) should be given as an auxiliary entry in the config:
 
     .. code-block:: python
 
-        cfg.x.dy_config = DrellYanConfig(
+        cfg.x.dy_weight_config = DrellYanConfig(
             era="2022preEE_NLO",
             correction="DY_pTll_reweighting",
             unc_correction="DY_pTll_reweighting_N_uncertainty",
         )
 
-    *get_dy_config* can be adapted in a subclass in case it is stored differently in the config.
+    *get_dy_weight_config* can be adapted in a subclass in case it is stored differently in the config.
     """
 
     # map the input variable names from the corrector to our columns
@@ -121,14 +119,10 @@ def dy_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     # initializing the list of weight variations
     weights_list = [("dy_weight", "nom")]
 
-    # determining the number of uncertainties (dependent on the era)
-    inputs_unc = [variable_map[inp.name] for inp in self.dy_unc_corrector.inputs]
-    dy_n_unc = int(self.dy_unc_corrector.evaluate(*inputs_unc))
-
     # appending the respective number of uncertainties to the weight list
-    for i in range(1, dy_n_unc + 1):
+    for i in range(self.n_unc):
         for shift in ("up", "down"):
-            tmp_tuple = (f"dy_weight_{shift}{i}", f"{shift}{i}")
+            tmp_tuple = (f"dy_weight{i + 1}_{shift}", f"{shift}{i + 1}")
             weights_list.append(tmp_tuple)
 
     # preparing the input variables for the corrector
@@ -143,6 +137,20 @@ def dy_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
         events = set_ak_column(events, column_name, dy_weight, value_type=np.float32)
 
     return events
+
+
+@dy_weights.init
+def dy_weights_init(self: Producer) -> None:
+    # the number of weights in partial run 3 is always 10
+    if self.config_inst.campaign.x.year not in (2022, 2023):
+        raise NotImplementedError(
+            f"campaign year {self.config_inst.campaign.x.year} is not yet supported by {self.cls_name}",
+        )
+    self.n_unc = 10
+
+    # register dynamically produced weight columns
+    for i in range(self.n_unc):
+        self.produces.add(f"dy_weight{i + 1}_{{up,down}}")
 
 
 @dy_weights.requires
@@ -176,7 +184,7 @@ def dy_weights_setup(
     import correctionlib
     correctionlib.highlevel.Correction.__call__ = correctionlib.highlevel.Correction.evaluate
     correction_set = correctionlib.CorrectionSet.from_string(
-        self.get_dy_file(bundle.files).load(formatter="gzip").decode("utf-8"),
+        self.get_dy_weight_file(bundle.files).load(formatter="gzip").decode("utf-8"),
     )
 
     # check number of fetched correctors
@@ -184,6 +192,12 @@ def dy_weights_setup(
         raise Exception("Expected exactly two types of Drell-Yan correction")
 
     # create the weight and uncertainty correctors
-    self.dy_config: DrellYanConfig = self.get_dy_config()
+    self.dy_config: DrellYanConfig = self.get_dy_weight_config()
     self.dy_corrector = correction_set[self.dy_config.correction]
-    self.dy_unc_corrector = correction_set[self.dy_config.unc_correction]
+
+    dy_n_unc = int(self.dy_unc_corrector.evaluate(self.dy_config.era))
+
+    if dy_n_unc != self.n_unc:
+        raise ValueError(
+            f"Expected {self.n_unc} uncertainties, got {dy_n_unc}",
+        )
