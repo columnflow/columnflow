28c0a816ee04fa2a0ffe6010e1097882cf446971
Nathan Prouvost <49162277+nprouvost@users.noreply.github.com>
2025-04-14 13:34:29 +0200

    implement own errorbar calculation (#675)
    * implement own errorbar calculation
    
    * make poisson error calculation independent of histogram shape
    
    * Apply suggestions from code review
    
    Co-authored-by: Marcel Rieger <riga@users.noreply.github.com>
    
    * change function name
    
    * Apply comments from review
    
    Co-authored-by: Mathis Frahm <49306645+mafrahm@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Marcel Rieger <riga@users.noreply.github.com>
    Co-authored-by: Mathis Frahm <49306645+mafrahm@users.noreply.github.com>

--- columnflow/plotting/plot_util.py
+++ columnflow/plotting/plot_util.py
@@ -565,6 +565,7 @@ def prepare_stack_plot_config(
                 "norm": line_norm,
                 "label": line_labels[i],
                 "color": line_colors[i],
+                "error_type": "variance",
             },
             # "ratio_kwargs": {
             #     "norm": h.values(),
@@ -588,7 +589,7 @@ def prepare_stack_plot_config(
             "ratio_kwargs": {"norm": h_mc.values()},
         }
 
-    # draw systemetic error for stack
+    # draw systematic error for stack
     if h_mc_stack is not None and mc_syst_hists:
         mc_norm = sum(h_mc.values()) if shape_norm else 1
         plot_config["mc_syst_unc"] = {
@@ -616,12 +617,14 @@ def prepare_stack_plot_config(
             "kwargs": {
                 "norm": data_norm,
                 "label": data_label or "Data",
+                "error_type": "poisson_unweighted",
             },
         }
 
         if h_mc is not None:
             plot_config["data"]["ratio_kwargs"] = {
                 "norm": h_mc.values() * data_norm / mc_norm,
+                "error_type": "poisson_unweighted",
             }
 
         # suppress error bars by overriding `yerr`
@@ -1019,3 +1022,52 @@ def remove_label_placeholders(
         sel = "[A-Z0-9]+"
 
     return re.sub(f"__{sel}__", "", label)
+
+
+def calculate_stat_error(
+    hist: hist.Hist,
+    error_type: str,
+) -> dict:
+    """
+    Calculate the error to be plotted for the given histogram *hist*.
+    Supported error types are:
+        - 'variance': the plotted error is the square root of the variance for each bin
+        - 'poisson_unweighted': the plotted error is the poisson error for each bin
+        - 'poisson_weighted': the plotted error is the poisson error for each bin, weighted by the variance
+    """
+
+    # determine the error type
+    if error_type == "variance":
+        yerr = hist.view().variance ** 0.5
+    elif error_type in {"poisson_unweighted", "poisson_weighted"}:
+        # compute asymmetric poisson confidence interval
+        from hist.intervals import poisson_interval
+
+        variances = hist.view().variance if error_type == "poisson_weighted" else None
+        values = hist.view().value
+        confidence_interval = poisson_interval(values, variances)
+
+        if error_type == "poisson_weighted":
+            # might happen if some bins are empty, see https://github.com/scikit-hep/hist/blob/5edbc25503f2cb8193cc5ff1eb71e1d8fa877e3e/src/hist/intervals.py#L74  # noqa: E501
+            confidence_interval[np.isnan(confidence_interval)] = 0
+        elif np.any(np.isnan(confidence_interval)):
+            raise ValueError("Unweighted Poisson interval calculation returned NaN values, check Hist package")
+
+        # calculate the error
+        # yerr_lower is the lower error
+        yerr_lower = values - confidence_interval[0]
+        # yerr_upper is the upper error
+        yerr_upper = confidence_interval[1] - values
+        # yerr is the size of the errorbars to be plotted
+        yerr = np.array([yerr_lower, yerr_upper])
+
+        if np.any(yerr < 0):
+            logger.warning(
+                "yerr < 0, setting to 0. "
+                "This should not happen, please check your histogram.",
+            )
+            yerr[yerr < 0] = 0
+    else:
+        raise ValueError(f"unknown error type '{error_type}'")
+
+    return yerr
