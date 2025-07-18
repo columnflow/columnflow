632ea294ac0d953bc72a05c53c0a68d677fe2820
Mathis Frahm <mathisfrahm@gmx.de>
2025-07-18 15:58:23 +0200

    improve flexibility & runtime of helper functions

--- columnflow/tasks/framework/histograms.py
+++ columnflow/tasks/framework/histograms.py
@@ -6,6 +6,8 @@ Base tasks for working with multiple merged histograms.
 
 from __future__ import annotations
 
+from functools import cached_property
+
 import law
 import order as od
 
@@ -40,35 +42,53 @@ class HistogramsUserBase(
         parts.insert_before("version", "datasets", f"datasets_{self.datasets_repr}")
         return parts
 
+    @cached_property
+    def inputs(self):
+        return self.input()
+
     def load_histogram(
         self,
         config: str | od.Config,
         dataset: str | od.Dataset,
         variable: str | od.Variable,
+        update_label: bool = True,
     ) -> hist.Hist:
         """
         Helper function to load the histogram from the input for a given dataset and variable.
 
+        :param config: The config name or instance.
         :param dataset: The dataset name or instance.
         :param variable: The variable name or instance.
+        :param update_label: Whether to update the label of the variable axis in the histogram.
+        If True, the label will be updated based on the first config instance's variable label.
         :return: The loaded histogram.
         """
+
         if isinstance(dataset, od.Dataset):
             dataset = dataset.name
         if isinstance(variable, od.Variable):
             variable = variable.name
         if isinstance(config, od.Config):
             config = config.name
-        histogram = self.input()[config][dataset]["collection"][0]["hists"].targets[variable].load(formatter="pickle")
+        histogram = self.inputs[config][dataset]["collection"][0]["hists"].targets[variable].load(formatter="pickle")
+
+        if update_label:
+            # get variable label from first config instance
+            for var_name in variable.split("-"):
+                label = self.config_insts[0].get_variable(var_name).x_title
+                ax_names = [ax.name for ax in histogram.axes]
+                if var_name in ax_names:
+                    # update the label of the variable axis
+                    histogram.axes[var_name].label = label
         return histogram
 
     def slice_histogram(
         self,
         histogram: hist.Hist,
         config_inst: od.Config,
-        processes: str | list[str],
-        categories: str | list[str],
-        shifts: str | list[str],
+        processes: str | list[str] | None = None,
+        categories: str | list[str] | None = None,
+        shifts: str | list[str] | None = None,
         reduce_axes: bool = False,
     ) -> hist.Hist:
         """
@@ -89,49 +109,56 @@ class HistogramsUserBase(
         def flatten_nested_list(nested_list):
             return [item for sublist in nested_list for item in sublist]
 
-        # transform into lists if necessary
-        processes = law.util.make_list(processes)
-        categories = law.util.make_list(categories)
-        shifts = law.util.make_list(shifts)
+        selection_dict = {}
 
-        # get all leaf categories
-        category_insts = list(map(config_inst.get_category, categories))
-        leaf_category_insts = set(flatten_nested_list([
-            category_inst.get_leaf_categories() or [category_inst]
-            for category_inst in category_insts
-        ]))
+        if processes:
+            # transform into lists if necessary
+            processes = law.util.make_list(processes)
+            # get all sub processes
 
-        # get all sub processes
-        process_insts = list(map(config_inst.get_process, processes))
-        sub_process_insts = set(flatten_nested_list([
-            [sub for sub, _, _ in proc.walk_processes(include_self=True)]
-            for proc in process_insts
-        ]))
+            process_insts = list(map(config_inst.get_process, processes))
+            sub_process_insts = set(flatten_nested_list([
+                [sub for sub, _, _ in proc.walk_processes(include_self=True)]
+                for proc in process_insts
+            ]))
+            selection_dict["process"] = [
+                hist.loc(p.name)
+                for p in sub_process_insts
+                if p.name in histogram.axes["process"]
+            ]
+        if categories:
+            # transform into lists if necessary
+            categories = law.util.make_list(categories)
 
-        # get all shift instances
-        shift_insts = [config_inst.get_shift(shift) for shift in shifts]
+            # get all leaf categories
+            category_insts = list(map(config_inst.get_category, categories))
+            leaf_category_insts = set(flatten_nested_list([
+                category_inst.get_leaf_categories() or [category_inst]
+                for category_inst in category_insts
+            ]))
+            selection_dict["category"] = [
+                hist.loc(c.name)
+                for c in leaf_category_insts
+                if c.name in histogram.axes["category"]
+            ]
+
+        if shifts:
+            # transform into lists if necessary
+            shifts = law.util.make_list(shifts)
+
+            # get all shift instances
+            shift_insts = [config_inst.get_shift(shift) for shift in shifts]
+            selection_dict["shift"] = [
+                hist.loc(s.name)
+                for s in shift_insts
+                if s.name in histogram.axes["shift"]
+            ]
 
         # work on a copy
         h = histogram.copy()
 
         # axis selections
-        h = h[{
-            "process": [
-                hist.loc(p.name)
-                for p in sub_process_insts
-                if p.name in h.axes["process"]
-            ],
-            "category": [
-                hist.loc(c.name)
-                for c in leaf_category_insts
-                if c.name in h.axes["category"]
-            ],
-            "shift": [
-                hist.loc(s.name)
-                for s in shift_insts
-                if s.name in h.axes["shift"]
-            ],
-        }]
+        h = h[selection_dict]
 
         if reduce_axes:
             # axis reductions
@@ -163,6 +190,7 @@ class HistogramsUserSingleShiftBase(
             config_inst.name: {
                 d: self.reqs.MergeHistograms.req_different_branching(
                     self,
+                    config=config_inst.name,
                     dataset=d,
                     branch=-1,
                     _prefer_cli={"variables"},
@@ -194,14 +222,15 @@ class HistogramsUserMultiShiftBase(
     def requires(self):
         datasets = [self.datasets] if self.single_config else self.datasets
         return {
-            config: {
+            config_inst.name: {
                 d: self.reqs.MergeShiftedHistograms.req_different_branching(
                     self,
+                    config=config_inst.name,
                     dataset=d,
                     branch=-1,
                     _prefer_cli={"variables"},
                 )
                 for d in datasets[i]
             }
-            for i, config in enumerate(self.configs)
+            for i, config_inst in enumerate(self.config_insts)
         }
