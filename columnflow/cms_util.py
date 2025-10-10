59d13bba3afc3a1e57b24d6301a767b49ce89838
Marcel R. <github.riga@icloud.com>
2025-10-10 08:56:26 +0200

    Add CMSDatasetInfo.

--- columnflow/cms_util.py
+++ columnflow/cms_util.py
@@ -9,6 +9,8 @@ from __future__ import annotations
 __all__ = []
 
 import os
+import re
+import copy
 import dataclasses
 
 from columnflow.types import ClassVar, Generator
@@ -83,3 +85,102 @@ class CATInfo:
             getattr(self.snapshot, pog.lower()),
             *(p.strip("/") for p in path),
         )
+
+
+@dataclasses.dataclass
+class CMSDatasetInfo:
+    """
+    Container to wrap a CMS dataset given by its *key* with access to its components. The key should be in the format
+    ``/<name>/<campaign>-<campaign_version>-<dataset_version>/<tier>AOD<mc:sim>``.
+
+    .. code-block:: python
+
+        d = CMSDatasetInfo.from_key("/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/RunIII2024Summer24MiniAODv6-150X_mcRun3_2024_realistic_v2-v2/MINIAODSIM") # noqa
+        print(d.name)              # TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8
+        print(d.campaign)          # RunIII2024Summer24MiniAODv6
+        print(d.campaign_version)  # 150X_mcRun3_2024_realistic_v2
+        print(d.dataset_version)   # v2
+        print(d.tier)              # mini (lower case)
+        print(d.mc)                # True
+        print(d.data)              # False
+        print(d.kind)              # mc
+    """
+    name: str
+    campaign: str
+    campaign_version: str
+    dataset_version: str  # this is usually the GT for MC
+    tier: str
+    mc: bool
+
+    @classmethod
+    def from_key(cls, key: str) -> CMSDatasetInfo:
+        """
+        Takes a dataset *key*, splits it into its components, and returns a new :py:class:`CMSDatasetInfo` instance.
+
+        :param key: The dataset key:
+        :return: A new instance of :py:class:`CMSDatasetInfo`.
+        """
+        # split
+        if not (m := re.match(r"^/([^/]+)/([^/-]+)-([^/-]+)-([^/-]+)/(.+)AOD(SIM)?$", key)):
+            raise ValueError(f"invalid dataset key '{key}'")
+
+        # create instance
+        return cls(
+            name=m.group(1),
+            campaign=m.group(2),
+            campaign_version=m.group(3),
+            dataset_version=m.group(4),
+            tier=m.group(5).lower(),
+            mc=m.group(6) == "SIM",
+        )
+
+    @property
+    def key(self) -> str:
+        # transform back to key format
+        return (
+            f"/{self.name}"
+            f"/{self.campaign}-{self.campaign_version}-{self.dataset_version}"
+            f"/{self.tier.upper()}AOD{'SIM' if self.mc else ''}"
+        )
+
+    @property
+    def data(self) -> bool:
+        return not bool(self.mc)
+
+    @data.setter
+    def data(self, value: bool) -> None:
+        self.mc = not bool(value)
+
+    @property
+    def kind(self) -> str:
+        return "mc" if self.mc else "data"
+
+    @kind.setter
+    def kind(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise TypeError(f"expected string for kind, got '{value}' ({type(value)})")
+        if (_value := value.lower()) not in {"mc", "data"}:
+            raise ValueError(f"invalid kind '{value}', expected 'mc' or 'data'")
+        self.mc = _value == "mc"
+
+    @property
+    def store_path(self) -> str:
+        return (
+            "/store"
+            f"/{self.kind}"
+            f"/{self.campaign}"
+            f"/{self.name}"
+            f"/{self.tier.upper()}AOD{'SIM' if self.mc else ''}"
+            f"/{self.campaign_version}-{self.dataset_version}"
+        )
+
+    def copy(self, **kwargs) -> CMSDatasetInfo:
+        """
+        Creates a copy of this instance, allowing to override specific attributes via *kwargs*.
+
+        :param kwargs: Attributes to override in the copy.
+        :return: A new instance of :py:class:`CMSDatasetInfo`.
+        """
+        attrs = copy.deepcopy(self.__dict__)
+        attrs.update(kwargs)
+        return self.__class__(**attrs)
